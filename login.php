fcb5be8895c251e9c16368ea0706bc697fd248d9
Laurens K <l.kropf@icloud.com>
2020-02-03 21:33:13 +0100

    v2.2

--- login.php
+++ login.php
@@ -94,7 +94,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
                 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 
